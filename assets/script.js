a138d0ff23fafb7684fa85a585edcd9d526dbbaf
ch-wannaporn <ch.wannaporn_st@tni.ac.th>
2021-12-18 11:11:53 +0700

    set width last child

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,13 @@ fetch('https://picsum.photos/v2/list')
     .then(res=>res.json())
     .then(data=>{
         var images = data
-        images.forEach(item => {
+        images.forEach((item, index) => {
             var img = document.createElement('img')
             img.classList.add('frame')
             img.setAttribute('src', item.download_url)
+            if(index === images.length - 1) {
+                img.width('width', 150.0/item.height*item.width)
+            }
             container.appendChild(img)
         })
     })
