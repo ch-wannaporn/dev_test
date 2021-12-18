477a7ae2dfd284a7f7225120f7e8abc643e2180e
ch-wannaporn <ch.wannaporn_st@tni.ac.th>
2021-12-18 17:28:52 +0700

    function getBookingsForWeek is finished

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const bookingData = require('./data.json')
 
+Date.prototype.getWeekNumber = function(){
+    var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    var dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+};
+
 const checkAvailability = (roomId, startTime, endTime) => {
     const clientStartTime = new Date(startTime)
     const clientEndTime = new Date(endTime)
@@ -22,6 +30,8 @@ const getBookingsForWeek = (roomId, weekNo) => {
     const nextWeekBookings = []
 
     const today = new Date().setHours(0, 0, 0, 0)
+    const thisWeek = +weekNo
+    const nextWeek = thisWeek + 1
 
     const allBookings = bookingData.filter(item => {
         return item.roomId === roomId
@@ -30,15 +40,18 @@ const getBookingsForWeek = (roomId, weekNo) => {
     allBookings.forEach(item => {
         const startDate = new Date(item.startTime).setHours(0, 0, 0, 0)
         const endDate = new Date(item.endTime).setHours(0, 0, 0, 0)
-        const startWeekNo = new Date(item.startTime)
+        const startWeekNo = new Date(item.startTime).getWeekNumber()
+        const endWeekNo = new Date(item.endTime).getWeekNumber()
 
-        //return startDate === today || endDate === today
-    
+        if(startDate === today || endDate === today)
+            todayBookings.push(item)
+        if(startWeekNo === thisWeek || endWeekNo === thisWeek)
+            thisWeekBookings.push(item)
+        if(startWeekNo === nextWeek || endWeekNo === nextWeek)
+            nextWeekBookings.push(item)
     })
 
-    const roomBookings = {today: [], thisWeek: [], nextWeek: []}
-
-    return "hi"
+    return {today: todayBookings, thisWeek: thisWeekBookings, nextWeek: nextWeekBookings}
 }
 
 module.exports = {checkAvailability, getBookingsForWeek}
